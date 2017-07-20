2121223bb9a85da7abb7bfddf6ef25bb243f43e9
Septian A Tama <sfirst.tama@gmail.com>
2017-07-20 09:48:45 +0700

    fix me please

---
+++ models/Transaction.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var transactionSchema = mongoose.Schema({
+  memberid: String,
+  days: String,
+  date: { type: Date, default: Date.now },
+  price: Number,
+  booklist: [{ type: Schema.Types.ObjectId, ref: 'Book' }]
+});
+
+var Transaction = mongoose.model('Transaction', transactionSchema);
+
+module.exports = Transaction
