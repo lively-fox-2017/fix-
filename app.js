c85d02adbebea384fb14fdb8356e0fab633b15c2
NNcrawler <rasyid.xyz@gmail.com>
2017-11-09 10:17:38 +0700

    fixing book create route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const body-parser = require('body-parser');
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- controllers/books.js
+++ controllers/books.js
@@ -1,3 +1,5 @@
+const Book = require('../models/Book');
+
 module.exports = {
   all: function(req, res) {
     Book.find(function (err, books) {
@@ -8,7 +10,14 @@ module.exports = {
     })
   },
   create: function(req, res) {
-    var book = new Book(req.body);
+    const data = {
+      isbn: req.body.isbn,
+      title: req.body.title,
+      author: req.body.body,
+      category: req.body.category,
+      stock: req.body.stock
+    };
+    var book = new Book(data);
     book.save(function (err, result) {
       if (err) {
         res.send({err: err})
