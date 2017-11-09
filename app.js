a664c073a780e6770dbd597c3a91dd6d795cbbee
munawirali <munawirali@gmail.com>
2017-11-09 10:20:18 +0700

    feature books done

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 
 var mongoose = require('mongoose');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
