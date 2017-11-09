c0524bef42d23f7ad565196afd48644ff9239c0d
jasoneffendi <jasoneffendi27@gmail.com>
2017-11-09 10:14:57 +0700

    Changed req.id to req.params.id

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -20,7 +20,7 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Transaction.update({ _id: req.id }, {
+    Transaction.update({ _id: req.params.id }, {
       $set: req.body
     }, function(err, result) {
       if (err) {
@@ -30,7 +30,7 @@ module.exports = {
     });
   },
   delete: function(req, res) {
-    Transaction.remove({ _id: req.id }, function (err, result) {
+    Transaction.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.send({err: err})
       }
