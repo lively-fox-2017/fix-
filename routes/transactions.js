5fd4fb579aa9b1334021e5e16c61fb73da98ba2f
sisinduku <saptanto.sindu@gmail.com>
2017-11-09 10:06:04 +0700

    Fix not exported router

--- routes/transactions.js
+++ routes/transactions.js
@@ -6,3 +6,5 @@ router.get('/', transactionController.all)
 router.post('/', transactionController.create)
 router.put('/:id', transactionController.update)
 router.delete('/:id', transactionController.delete)
+
+module.exports = router
