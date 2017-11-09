d9f54745de488459bc8ee3ca318e0db9616c5d00
nach9 <nur.achdiansyah@gmail.com>
2017-11-09 10:23:37 +0700

    fix del trx ctrl

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -37,6 +37,14 @@ module.exports = {
         res.send({err: err})
       }
       res.send(result)
-    }
-  });
+    })
+  }
+  // delete: function(req, res) {
+  //   Transaction.remove({ _id: req.id }, function (err, result) {
+  //     if (err) {
+  //       res.send({err: err})
+  //     }
+  //     res.send(result)
+  //   }
+  // });
 }
